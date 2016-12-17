733c5923a80f1d7b4ff532514ed12a0899fc2b1f
KumanakaRyota <kumanaka-ryota@av.avex.co.jp>
2016-12-17 05:12:16 +0000

    実習４まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #--********************* 下記を追加 *********************
+  before_action :authenticate_user!
   
 #--**************************** 下記を追加 ****************************
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -11,6 +14,7 @@ class ApplicationController < ActionController::Base
     # 新規作成にname、thumbnail、agreementパラメータを含める
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :thumbnail, :agreement])
   end
+
 #--*******************************************************************  
   
 end
