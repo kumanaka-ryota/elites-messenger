b274fd8b657f3f94e7b3e3538ebf2fcfdecb2a3e
KumanakaRyota <kumanaka-ryota@av.avex.co.jp>
2017-01-08 08:05:12 +0000

    実習１２まで完了

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,8 +1,13 @@
 class TimelinesController < ApplicationController
   def index
     @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
-    @timeline = Timeline.includes(:user).user_filter(params[:filter_user_id]).order('updated_at DESC')
+    @timeline = Timeline.includes(:user).not_reply.user_filter(params[:filter_user_id]).order('updated_at DESC')
     @users = User.all
+
+    if params[:reply_id]
+      # 返信時は返信のタイムライン情報を取得
+      @reply_timeline = Timeline.find(params[:reply_id])
+    end
 
   end
 
@@ -40,6 +45,6 @@ class TimelinesController < ApplicationController
   
   private
   def input_message_param
-    params.require(:timeline).permit(:message)
+    params.require(:timeline).permit(:message, :reply_id)
   end  
 end
