d5e72115613dc4b50618682064e9ebd6bdfc72ca
KumanakaRyota <kumanaka-ryota@av.avex.co.jp>
2017-03-12 09:00:27 +0000

    ajaxエラー表示処理修正

--- app/assets/javascripts/timelines.js
+++ app/assets/javascripts/timelines.js
@@ -8,34 +8,21 @@ $(function(){
   });
 
   $('form.input_message_form').on('ajax:complete', function(event, data, status){
+    var json,
+        errors;    
     // Ajaxレスポンス
-    if ( status == 'success') {
+    if(status == 'success') {    
       var json = JSON.parse(data.responseText);
-    // 基本課題15　ADD
-      // $('div.timeline').prepend(json.timeline);
-      // 正常な場合はjson.timelineを挿入
-      // console.log(json);
-      // $('div.timeline').prepend(json.timeline);
-      if (json.timeline) {
-        $('div.alert').empty();
-        $('div.timeline').prepend(json.timeline);
+      if(json.timeline) {
+        $('div.timeline').prepend(json.timeline);  
+        $('div.alert').html('')
+        $('#timeline_message').val('');
+      } else if (json.errors) {
+        $('div.alert').html('')
+        json.errors.forEach(function(error) {
+          $('<p>', {class: 'alert', text: error}).appendTo('div.alert');
+        });
       }
-      else if (json.errors) {
-        $('div.alert').empty();
-        $('div.alert').append(json.errors);
-      }
-      else {
-        $('div.alert').html('エラーが発生しました');
-      }
-    }
-    // エラーが発生した場合はエラー内容を挿入
-    else{
-      // ??? timelineではダメ。ajaxを学習してから修正すること！
-      // $('div.timeline').prepend(json.timeline);
-      // var json = JSON.parse(data.responseText);
-      // $('div.actions').append(json.timeline).css('color','red');
-      // ここに書くのがそもそも間違い。validationエラーでもsuccessなので。
-      $('div.alert').html('エラーが発生しました');
     }
   });
 });
